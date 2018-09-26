3a74d77a557dbf0102f44f100124f2c75ddeabb4
Jack Hallybone <jackhallybone@users.noreply.github.com>
2018-09-26 16:34:47 +0100

    blank iOS spa xcode project

---
+++ GlanceCal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GlanceCal
+//
+//  Created by Jack Hallybone on 26/09/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
